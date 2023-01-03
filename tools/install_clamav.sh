6a6123d8cde1f678332b2bcba909081a64a997b3
fteuti <85801364+fteuti@users.noreply.github.com>
2023-01-03 18:36:23 +0100

    Update install_clamav.sh

--- tools/install_clamav.sh
+++ tools/install_clamav.sh
@@ -6,14 +6,14 @@ function error {
 }
 
 echo "Creating directories..."
-sudo mkdir -p /portainer/Files/AppData/Config/clamav/config || error "Failed to create clamav config folder!"
-sudo mkdir -p /portainer/Files/AppData/Config/clamav/virus_definitions || error "Failed to create clamav virus_definitions folder!"
+sudo mkdir -p /portainer/clamav/config || error "Failed to create clamav config folder!"
+sudo mkdir -p /portainer/clamav/virus_definitions || error "Failed to create clamav virus_definitions folder!"
 
 echo "Creating pre-filled clamav config files"
-sudo wget -O /portainer/Files/AppData/Config/clamav/config/clamd.conf https://github.com/Cisco-Talos/clamav/files/10096744/clamd.conf.txt || error "Failed to get clamd config file!"
-sudo wget -O /portainer/Files/AppData/Config/clamav/config/freshclam.conf https://github.com/Cisco-Talos/clamav/files/10096745/freshclam.conf.txt || error "Failed to get freshclam config file"
+sudo wget -O /portainer/clamav/config/clamd.conf https://github.com/Cisco-Talos/clamav/files/10096744/clamd.conf.txt || error "Failed to get clamd config file!"
+sudo wget -O /portainer/clamav/config/freshclam.conf https://github.com/Cisco-Talos/clamav/files/10096745/freshclam.conf.txt || error "Failed to get freshclam config file"
 
 echo "Setting the right permissions"
-sudo chown -R 100:101 /portainer/Files/AppData/Config/clamav/ || error "Unable to set the right permissions for clamav!"
+sudo chown -R 100:101 /portainer/clamav/ || error "Unable to set the right permissions for clamav!"
 
 echo "Setup complete. You can now install clamav using the App Template."
